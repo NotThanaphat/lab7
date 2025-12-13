e4993e88fa4a9f9d2121b5151a0c362824da01c4
NotThanaphat <thanapat.pingkaew@gmail.com>
2025-12-13 16:53:34 +0700

    upload lab7 22

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,26 +3,25 @@
 using namespace std;
 
 int main() {
-    string name, movie, day, msg;
-    int id;
+    string name, movie, day, lastmsg;
+    string id;
+    int gear;
 
     cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
     cout << "?????: ";
     getline(cin, name);
 
     cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
-
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
     cout << name << ": ";
     cin >> id;
-    cin.ignore();
 
-    int gear = (id / 10000000) - 12;
+    gear = stoi(id.substr(0, 2)) - 12;
 
-    cout << "Fahsai: I think you may be GEAR " << gear 
-         << ". I have a free movie ticket for you.\n";
-
+    cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
     cout << "Fahsai: Let's go to the cinema together!!!\n";
+
+    cin.ignore();
 
     cout << "Fahsai: What movie do you want to watch?\n";
     cout << name << ": ";
@@ -32,11 +31,11 @@ int main() {
     cout << name << ": ";
     getline(cin, day);
 
-    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching "
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " 
          << movie << " with you.\n";
 
     cout << name << ": ";
-    getline(cin, msg);
+    getline(cin, lastmsg);
 
     cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/\n";
 
